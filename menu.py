96c407ffa077c28cd062c2062bd2875d6846dd23
RadkaMat <104318039+RadkaMat@users.noreply.github.com>
2025-04-19 18:17:16 +0200

    Menu - debug shaking mobile view

--- menu.py
+++ menu.py
@@ -9,13 +9,13 @@ st.title('Portfolio')
 
 img_url = r'https://github.com/RadkaMat/Streamlit_apps/blob/master/web_app_data/to_do_list_img_big.jpg?raw=true'
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
     with st.container(border=True):
         st.image(img_url)
     st.page_link('pages/to-do_list.py', label='run To-do List App', icon='📝')
 
-with col3:
+with col2:
     with st.container(border=True):
         pass
